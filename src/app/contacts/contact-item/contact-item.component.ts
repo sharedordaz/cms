aef4789812f8a44642ef886b9d1573998739029f
sharedordaz <sharedordaz@live.com.mx>
2023-05-08 22:19:01 -0600

    Code cleaned

--- src/app/contacts/contact-item/contact-item.component.ts
+++ src/app/contacts/contact-item/contact-item.component.ts
@@ -6,9 +6,5 @@ import { Contact } from '../contacts.model';
   styleUrls: ['./contact-item.component.css']
 })
 export class ContactItemComponent {
-  /*contacts: Contact[] = [
-    new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', 'null'),
-    new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', 'null')
-  ];*/
   @Input() contact?: any;
 }
