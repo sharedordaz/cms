ba1c087412473727d8167cee11b37559bf133a17
sharedordaz <sharedordaz@live.com.mx>
2023-05-09 23:12:41 -0600

    Reading the items from the inputs at message edit component

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-edit',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent {
+
+  @Output() addMessageEvent = new EventEmitter<any>;
+  @ViewChild("subject") subjectInput!: ElementRef<HTMLInputElement>;
+  @ViewChild("msgText") msgTextInput!: ElementRef<HTMLInputElement>;
+
+  onSendMessage(subject: HTMLInputElement, msgText: HTMLInputElement) {
+    let currentSender: Message = new Message(200, subject.value, msgText.value, "Myself");
+    this.addMessageEvent.emit(currentSender);
+    alert("onSendMessage" + currentSender.subject);
+  }
+  onClear() {
+    alert("onSendMessage");
+
+  }
+
 
 }
