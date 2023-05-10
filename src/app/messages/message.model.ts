4017634ed4a8975ee5a829b24bd7bc6fc206f966
sharedordaz <sharedordaz@live.com.mx>
2023-05-09 20:25:35 -0600

    Created documents.model and message.model

---
+++ src/app/messages/message.model.ts
@@ -0,0 +1,19 @@
+/**
+ Message TS model
+ **/
+export class Document {
+
+  public id: string;
+  public subject: string;
+  public msgText: string;
+  public sender: string;
+
+  constructor(id: string, subject: string, msgText: string, sender: string) {
+    this.id = id;
+    this.subject = subject;
+    this.msgText = msgText;
+    this.sender = sender;
+
+  }
+
+}
