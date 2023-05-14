78e5fa267e5865ed162d6e6cd45cd82f9d7e99b5
sharedordaz <sharedordaz@live.com.mx>
2023-05-13 19:26:48 -0600

    Added the default templates

--- src/app/documents/document-detail/document-detail.component.ts
+++ src/app/documents/document-detail/document-detail.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-detail',
   templateUrl: './document-detail.component.html',
   styleUrls: ['./document-detail.component.css']
 })
-export class DocumentDetailComponent {
+export class DocumentDetailComponent implements OnInit {
+  //document coming as input
+  @Input() document?: Document;
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/documents/document-item/document-item.component.ts
+++ src/app/documents/document-item/document-item.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-item',
   templateUrl: './document-item.component.html',
   styleUrls: ['./document-item.component.css']
 })
-export class DocumentItemComponent {
+export class DocumentItemComponent implements OnInit {
+  //input for document
+  @Input() document?: Document;
 
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
 }
