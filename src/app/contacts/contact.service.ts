41628e014510e3b385f245cd14076c63720a8386
sharedordaz <sharedordaz@live.com.mx>
2023-05-23 16:42:58 -0600

    Worked on contact service

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Contact } from './contacts.model';
+import { MOCKCONTACTS } from "./MOCKCONTACTS";
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +8,10 @@ import { Contact } from './contacts.model';
 export class ContactService {
   contacts: Contact[] = [];
 
-  constructor() { }
+  constructor() {
+    this.contacts = MOCKCONTACTS;
+  }
+  getContacts(): Contact[] {
+    return this.contacts;
+  }
 }
--- src/app/contacts/contacts.model.ts
+++ src/app/contacts/contacts.model.ts
@@ -8,9 +8,9 @@ export class Contact {
   public email: string;
   public phone: string;
   public imageUrl: string;
-  public group: string;
+  public group: Contact[] | null;
 
-  constructor(id: string, name: string, email: string, phone: string, imageUrl: string, group: string) {
+  constructor(id: string, name: string, email: string, phone: string, imageUrl: string, group: Contact[] | null) {
     this.id = id;
     this.name = name;
     this.email = email
