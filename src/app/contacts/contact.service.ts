cf01f30bc536727f2eca442ea1fdfe605142eab3
sharedordaz <sharedordaz@live.com.mx>
2023-05-23 17:57:58 -0600

    Adding a loop

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -12,6 +12,19 @@ export class ContactService {
     this.contacts = MOCKCONTACTS;
   }
   getContacts(): Contact[] {
-    return this.contacts;
+    let returnedContact: Contact[] = this.contacts;
+    return returnedContact;
+  }
+
+  getContact(id: string): any | Contact | null {
+    for (let i = 0; i < this.contacts.length; i++) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+      else {
+        return null;
+      }
+
+    }
   }
 }
