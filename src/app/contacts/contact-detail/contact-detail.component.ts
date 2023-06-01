044bb0fc917cdcacc0156a91fe85fd7684236f79
sharedordaz <sharedordaz@live.com.mx>
2023-06-01 00:48:17 -0600

    Boring commit

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Contact } from '../contacts.model';
+import { ContactService } from '../contact.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-contact-detail',
@@ -8,11 +10,27 @@ import { Contact } from '../contacts.model';
 })
 export class ContactDetailComponent {
 
-  @Input() contact?: any;
+  contact?: any;
 
-  constructor() {
+  constructor(private contactService: ContactService, private router: Router, private route: ActivatedRoute) {
   }
+
   ngOnInit(): void {
+    //subscribe to current active route and get the id from param
+    this.route.params.subscribe(
+      (params: Params) => {
+        //retrieve contact that has id from params and store it in contact property
+        this.contact = this.contactService.getContact(params['id']);
+      }
+    )
+  }
+
+  onDelete() {
+    //delete using service
+    this.contactService.deleteContact(this.contact);
+    //navigate to contact list relative to this route
+    // this.router.navigate(['/contacts'], { relativeTo: this.route });
+    this.router.navigateByUrl('/contacts');
     //Here you put what is done when the constructor has been called
   }
 
