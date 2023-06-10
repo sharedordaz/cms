59bb81157ac402ad78ddfbad0141ef8c686e3198
sharedordaz <sharedordaz@live.com.mx>
2023-06-09 20:39:11 -0600

    At least it works

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { Document } from '../document.model';
 import { DocumentsService } from '../documents.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
-export class DocumentListComponent implements OnInit {
+export class DocumentListComponent implements OnInit, OnDestroy {
   @Output() selectedDocumentEvent = new EventEmitter<any>();
+
+  private subscription?: Subscription;
 
   documents: Document[] = []
   constructor(private documentService: DocumentsService) {
@@ -17,7 +20,7 @@ export class DocumentListComponent implements OnInit {
 
   ngOnInit(): void {
     this.documents = this.documentService.getDocuments();
-    this.documentService.documentChangedEvent.subscribe(
+    this.subscription = this.documentService.documentListChangedEvent.subscribe(
       (documents: Document[]) => {
         this.documents = documents;
       }
@@ -27,5 +30,10 @@ export class DocumentListComponent implements OnInit {
   onSelectedDocument(document: Document) {
     this.selectedDocumentEvent.emit(document);
   }
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+
+
 
 }
--- src/app/documents/documents.service.ts
+++ src/app/documents/documents.service.ts
@@ -1,16 +1,26 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Document } from './document.model';
 import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentsService {
   documents: Document[];
-  documentChangedEvent = new EventEmitter<Document[]>();
+  // //event emiter for changes in the document
+  // documentChangedEvent = new EventEmitter<Document[]>();
+
+  //property for max id
+  maxDocumentId?: number;
+
+  //subject property
+  documentListChangedEvent = new Subject<Document[]>();
+
 
   constructor() {
     this.documents = MOCKDOCUMENTS;
+    this.maxDocumentId = this.getMaxId();
   }
 
   getDocuments(): Document[] {
@@ -25,6 +35,70 @@ export class DocumentsService {
     }
     return null;
   }
+  //method to get max id number in document list
+  getMaxId(): number {
+    //variable to hold max Id
+    let maxId: number = 0;
+    //loop through the document list
+    for (const document of this.documents) {
+      //get current id as a number
+      const currentId: number = +document.id;
+      //if the current id is greater than max ID...
+      if (currentId > maxId) {
+        //then max id is the current id
+        maxId = currentId;
+      }
+    }
+    //return that max id
+    return maxId;
+  }
+
+  //method to add a document when user press add button
+  addDocument(newDocument: Document) {
+    //if null or undef...
+    if (newDocument === null || newDocument === undefined || this.maxDocumentId === undefined) {
+      //exit function
+      return;
+    }
+
+    //if document exists..
+    //increment id number and assign to new document (as string)
+    this.maxDocumentId++;
+    newDocument.id = this.maxDocumentId.toString();
+    //push unto list
+    this.documents.push(newDocument);
+    //create copy of list and emit/signal a change passing the copy
+    const documentListClone = this.documents.slice();
+    this.documentListChangedEvent.next(documentListClone);
+  }
+
+  //method to update/replace an existing document
+  updateDocument(originalDocument: Document, newDocument: Document) {
+    //check if document exists...
+    if (originalDocument === null || originalDocument === undefined || newDocument === null || newDocument === undefined) {
+      //if not, exit function
+      return;
+    }
+
+    //find position/index of original document
+    const pos = this.documents.indexOf(originalDocument);
+    //if the position is less than 0 (meaning it is not in the list)...
+    if (pos < 0) {
+      //exit
+      return;
+    }
+
+    //set the id of new document to be tht of the original
+    newDocument.id = originalDocument.id;
+    //set the document in the list to be the new document
+    let document: any;
+    document[pos] = newDocument;
+    //create copy
+    const documentListClone = this.documents.slice();
+    //emit/signal a change passing the copy
+    this.documentListChangedEvent.next(documentListClone);
+  }
+
   deleteDocument(document: Document) {
     //check if an existent document was passed
     if (document === null || document === undefined) {
@@ -40,6 +114,6 @@ export class DocumentsService {
     //removed document at specified position
     this.documents.splice(pos, 1);
     //emit event to signal that a change has been made, and pass it a new copy of the document list
-    this.documentChangedEvent.emit(this.documents.slice());
+    this.documentListChangedEvent.next(this.documents.slice());
   }
 }
