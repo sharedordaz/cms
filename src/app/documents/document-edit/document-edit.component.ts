62799fa6bb9aa4fb3c73ae284460dbeb8eb04707
sharedordaz <sharedordaz@live.com.mx>
2023-06-16 22:32:17 -0600

    This is the commit i make before the global warming makes us to move away from our houses and die from hunger

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DocumentsService } from "../documents.service";
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-edit',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent {
+
+  document?: Document | null;
+  editMode: boolean = false;
+  originalDocument?: Document | null;
+
+  constructor(
+    private documentsService: DocumentsService,
+    private router: Router,
+    private route: ActivatedRoute) {
+
+
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        const id: string = params["id"]
+        //if id parameter is undefined or null then
+        if (!id) {
+          this.editMode = false
+          return
+        }
+        if (this.originalDocument === undefined || this.originalDocument === null) {
+          return
+        }
+        else {
+          this.originalDocument = this.documentsService.getdocument(id)
+          this.editMode = true;
+          this.document = JSON.parse(JSON.stringify(this.originalDocument));
+
+        }
+      })
+  }
+  onCancel() {
+    this.router.navigate(['/documents'], { relativeTo: this.route });
+  }
 
 }
