ad57e61cb828b077483b58fd335f95285645e401
sharedordaz <sharedordaz@live.com.mx>
2023-06-27 20:20:13 -0600

    The module doesnt work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,7 +44,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
